06980991a21dd4ac3d54f73b65c5a220e4ffbe0c
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-17 20:02:09 -0500

    Create_invoices_table migration configured and belongsTo relationship with clients implemented.

--- app/Invoice.php
+++ app/Invoice.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function client(){
+        return $this->belongsTo(Client::class);
+    }
 }
