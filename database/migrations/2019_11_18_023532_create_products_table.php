5c4bce5380b5ed743bb30d08c636d4573aaaf571
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-17 22:06:08 -0500

    create_products_table migration configured

--- database/migrations/2019_11_18_023532_create_products_table.php
+++ database/migrations/2019_11_18_023532_create_products_table.php
@@ -16,6 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->softDeletes();
         });
     }
 
