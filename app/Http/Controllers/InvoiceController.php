e58eb7ce97e9a017d51be0c68a06e1571bc5ed80
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-17 19:49:09 -0500

    InvoiceController methods pointing to their respective views.

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,84 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use App\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return view('invoices.index', [
+            'invoices' => Invoice::all()
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('invoices.create', [
+            'invoice' => new Invoice,
+            'clients' => Client::all()
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        Invoice::create($request->all());
+
+        return redirect('/invoices')->with('message', 'Factura creada satisfactoriamente');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Invoice  $invoice
-     * @return \Illuminate\Http\Response
-     */
     public function show(Invoice $invoice)
     {
-        //
+        return view('invoices.show', [
+            'invoice' => $invoice
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Invoice  $invoice
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Invoice $invoice)
     {
-        //
+        return view('invoices.edit', [
+            'invoice' => $invoice,
+            'clients' => Client::all()
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Invoice  $invoice
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Invoice $invoice)
     {
-        //
+        $invoice->update($request->all());
+
+        return redirect()->route('invoices.show', $invoice)->with('message', 'Factura actualizada satisfactoriamente');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Invoice  $invoice
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Invoice $invoice)
     {
-        //
+        $invoice->delete();
+
+        return redirect()->route('invoices.index')->with('message', 'Factura eliminada satisfactoriamente');
     }
 }
