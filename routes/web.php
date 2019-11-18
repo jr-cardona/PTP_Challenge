c96d7fe4cc404b5e65a612e101e83015cb0c39af
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-17 20:10:40 -0500

    Invoice and Client resource routes configured with url in spanish

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('/facturas', 'InvoiceController')
+    ->names('invoices')
+    ->parameters(['facturas' => 'invoice']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/clientes', 'ClientController')
+    ->names('clients')
+    ->parameters(['clientes' => 'client']);
