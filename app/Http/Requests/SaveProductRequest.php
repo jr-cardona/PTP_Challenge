89f2a94caa09fd3a6402beb90efe0c3dbf3314c2
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-17 21:54:24 -0500

    Form Request created on Products

---
+++ app/Http/Requests/SaveProductRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaveProductRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:20',
+            'description' => 'required|max:255',
+        ];
+    }
+}
