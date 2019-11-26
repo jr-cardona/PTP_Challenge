90aa63752f14539c57eb508994e2e08c0bc62e5d
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-26 16:05:02 -0500

    JS assets configured for confirmDelete form

---
+++ public/js/delete-modal.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1:function(o,e,n){o.exports=n("wjEl")},wjEl:function(o,e){$("#confirmDeleteModal").on("show.bs.modal",(function(o){$("#deleteForm").attr("action",$(o.relatedTarget).data("route"))}))}},[[1,0]]]);
