9b2167fc69fafaaf48e08e5490771e8893ceb612
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-27 23:09:55 -0500

    All index views extends to layouts\index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,53 +1,35 @@
-@extends('layouts.app')
-@section('title', 'Productos')
-@section('content')
-    <div class="row">
-        <div class="col">
-            <h1>Productos</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <a class="btn btn-success" href="{{ route('products.create') }}">Crear nuevo producto</a>
-        </div>
-    </div>
-    <br>
-    {{ $products->links() }}
-    <table class="table border-rounded table-striped table-hover">
-        <thead class="thead-dark">
-            <tr style="text-align: center;">
-                <th scope="col">Código</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Descripción</th>
-                <th scope="col" nowrap>Fecha de creación</th>
-                <th scope="col" nowrap>Fecha de modificación</th>
-                <th scope="col" nowrap>Opciones</th>
-
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($products as $product)
-                <tr style="text-align: center;">
-                    <td>{{ $product->id }}</td>
-                    <td>
-                        <a href="{{ route('products.show', $product) }}">
-                            {{ $product->name }}
-                        </a>
-                    </td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->created_at }}</td>
-                    <td>{{ $product->updated_at }}</td>
-                    <td class="btn-group btn-group-sm" nowrap>
-                        @include('products._buttons')
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+@extends('layouts.index')
+@section('Title', 'Productos')
+@section('Name', 'Productos')
+@section('Create')
+    <a class="btn btn-success" href="{{ route('products.create') }}">Crear nuevo producto</a>
 @endsection
-@push('modals')
-    @include('partials.__confirm_delete_modal')
-@endpush
-@push('scripts')
-    <script src="{{ asset(mix('js/delete-modal.js')) }}"></script>
-@endpush
+@section('Links')
+    {{ $products->links() }}
+@endsection
+@section('Header')
+    <th scope="col">Código</th>
+    <th scope="col">Nombre</th>
+    <th scope="col">Descripción</th>
+    <th scope="col" nowrap>Fecha de creación</th>
+    <th scope="col" nowrap>Fecha de modificación</th>
+    <th scope="col" nowrap>Opciones</th>
+@endsection
+@section('Body')
+    @foreach($products as $product)
+        <tr class="text-center">
+            <td>{{ $product->id }}</td>
+            <td>
+                <a href="{{ route('products.show', $product) }}">
+                    {{ $product->name }}
+                </a>
+            </td>
+            <td>{{ $product->description }}</td>
+            <td nowrap>{{ $product->created_at }}</td>
+            <td nowrap>{{ $product->updated_at }}</td>
+            <td class="btn-group btn-group-sm" nowrap>
+                @include('products._buttons')
+            </td>
+        </tr>
+    @endforeach
+@endsection
--- resources/views/sellers/index.blade.php
+++ resources/views/sellers/index.blade.php
@@ -1,52 +1,35 @@
-@extends('layouts.app')
-@section('title', 'Vendedores')
-@section('content')
-    <div class="row">
-        <div class="col">
-            <h1>Vendedores</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <a class="btn btn-success" href="{{ route('sellers.create') }}">Crear nuevo vendedor</a>
-        </div>
-    </div>
-    <br>
-    {{ $sellers->links() }}
-    <table class="table border-rounded table-striped table-hover">
-        <thead class="thead-dark">
-            <tr class="text-center">
-                <th scope="col">Documento</th>
-                <th scope="col" nowrap>Nombre</th>
-                <th scope="col">Dirección</th>
-                <th scope="col">Correo electrónico</th>
-                <th scope="col">Celular</th>
-                <th scope="col" colspan="2">Opciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($sellers as $seller)
-                <tr class="text-center">
-                    <td>
-                        <a href="{{ route('sellers.show', $seller) }}">
-                            {{ $seller->type_document->name }} {{ $seller->document }}
-                        </a>
-                    </td>
-                    <td>{{ $seller->name }}</td>
-                    <td>{{ $seller->address }}</td>
-                    <td>{{ $seller->email }}</td>
-                    <td>{{ $seller->cell_phone_number }}</td>
-                    <td class="btn-group btn-group-sm" nowrap>
-                        @include('sellers._buttons')
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+@extends('layouts.index')
+@section('Title', 'Vendedores')
+@section('Name', 'Vendedores')
+@section('Create')
+    <a class="btn btn-success" href="{{ route('sellers.create') }}">Crear nuevo vendedor</a>
 @endsection
-@push('modals')
-    @include('partials.__confirm_delete_modal')
-@endpush
-@push('scripts')
-    <script src="{{ asset(mix('js/delete-modal.js')) }}"></script>
-@endpush
+@section('Links')
+    {{ $sellers->links() }}
+@endsection
+@section('Header')
+    <th scope="col">Documento</th>
+    <th scope="col" nowrap>Nombre</th>
+    <th scope="col">Dirección</th>
+    <th scope="col">Correo electrónico</th>
+    <th scope="col">Celular</th>
+    <th scope="col" colspan="2">Opciones</th>
+@endsection
+@section('Body')
+    @foreach($sellers as $seller)
+        <tr class="text-center">
+            <td>
+                <a href="{{ route('sellers.show', $seller) }}">
+                    {{ $seller->type_document->name }} {{ $seller->document }}
+                </a>
+            </td>
+            <td>{{ $seller->name }}</td>
+            <td>{{ $seller->address }}</td>
+            <td>{{ $seller->email }}</td>
+            <td>{{ $seller->cell_phone_number }}</td>
+            <td class="btn-group btn-group-sm" nowrap>
+                @include('sellers._buttons')
+            </td>
+        </tr>
+    @endforeach
+@endsection
