6684d2f52e0cf019a2fa5766c912d87526eb33c0
Juan Ricardo <jucardonaa@unal.edu.co>
2019-11-27 23:05:42 -0500

    Added layouts for index and show views

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('title')
+    @yield('Title')
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col">
+            <h1>@yield('Name')</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            @yield('Create')
+        </div>
+    </div>
+    <br>
+    @yield('Links')
+    <table class="table border-rounded table-striped table-hover">
+        <thead class="thead-dark">
+            <tr class="text-center">
+                @yield('Header')
+            </tr>
+        </thead>
+        <tbody>
+            @yield('Body')
+        </tbody>
+    </table>
+@endsection
+@push('modals')
+    @include('partials.__confirm_delete_modal')
+@endpush
+@push('scripts')
+    <script src="{{ asset(mix('js/delete-modal.js')) }}"></script>
+@endpush
---
+++ resources/views/layouts/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('title')
+    @yield('Title')
+@endsection
+@section('content')
+    @yield('Back')
+    <p></p>
+    <div class="card card-default">
+        <div class="card-header d-flex justify-content-between">
+            <h3 class="card-title mb-0">@yield('Name')</h3>
+            <div>
+                <div class="btn-group btn-group-sm">
+                    @yield('Buttons')
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            @yield('Body')
+        </div>
+    </div>
+@endsection
+@push('modals')
+    @include('partials.__confirm_delete_modal')
+@endpush
+@push('scripts')
+    <script src="{{ asset('js/delete-modal.js') }}"></script>
+@endpush
