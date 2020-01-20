90f14104ef0f0694508b562601ffef204d95d1fd
Juan Ricardo <jucardonaa@unal.edu.co>
2020-01-20 08:29:03 -0500

    Design improvements on all show views.

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,63 +1,81 @@
 @extends('layouts.show')
 @section('Title', 'Ver Cliente')
 @section('Back')
-    <a href="{{ route('clients.index') }}" class="btn btn-secondary">
-        <i class="fa fa-arrow-left"></i> {{ __("Volver") }}
-    </a>
+    <div>
+        <a href="{{ route('clients.index') }}" class="btn btn-secondary">
+            <i class="fa fa-arrow-left"></i> {{ __("Volver") }}
+        </a>
+    </div>
+    <div>
+        <a class="btn btn-success" href="{{ route('clients.create') }}">
+            <i class="fa fa-plus"></i> {{ __("Crear nuevo cliente") }}
+        </a>
+    </div>
 @endsection
 @section('Name')
-    {{ $client->name }}
+    {{ $client->fullname }}
 @endsection
 @section('Buttons')
     @include('clients._buttons')
 @endsection
 @section('Body')
-    <table class="table border-rounded table-sm">
-        <tr>
-            <td class="table-dark td-title">{{ __("Nombre completo:") }}</td>
-            <td class="td-content">{{ $client->fullname }}</td>
+    <div class="shadow">
+        <div class="card-header text-center"><h3>{{ __("Datos generales") }}</h3></div>
+        <table class="table border-rounded table-sm">
+            <tr>
+                <td class="table-dark td-title">{{ __("Tipo de documento:") }}</td>
+                <td class="td-content">{{ $client->type_document->fullname }}</td>
 
-            <td class="table-dark td-title">{{ __("Documento:") }}</td>
-            <td class="td-content">{{ $client->type_document->name }} {{ $client->document }}</td>
-        </tr>
-        <tr>
-            <td class="table-dark td-title">{{ __("Creado:")}}</td>
-            <td class="td-content">{{ $client->created_at }}</td>
+                <td class="table-dark td-title">{{ __("Número de documento:") }}</td>
+                <td class="td-content">{{ $client->document }}</td>
+            </tr>
+            <tr>
+                <td class="table-dark td-title">{{ __("Creado:")}}</td>
+                <td class="td-content">{{ $client->created_at->isoFormat('Y-MM-DD hh:mma') }}</td>
 
-            <td class="table-dark td-title">{{ __("Modificado:")}}</td>
-            <td class="td-content">{{ $client->updated_at }}</td>
-        </tr>
-        <tr>
-            <td class="table-dark td-title">{{ __("Número telefónico:")}}</td>
-            <td class="td-content">{{ $client->phone_number }}</td>
+                <td class="table-dark td-title">{{ __("Modificado:")}}</td>
+                <td class="td-content">{{ $client->updated_at->isoFormat('Y-MM-DD hh:mma') }}</td>
+            </tr>
+            <tr>
+                <td class="table-dark td-title">{{ __("Número telefónico:")}}</td>
+                <td class="td-content">{{ $client->phone_number }}</td>
 
-            <td class="table-dark td-title">{{ __("Celular:")}}</td>
-            <td class="td-content">{{ $client->cell_phone_number }}</td>
-        </tr>
-        <tr>
-            <td class="table-dark td-title">{{ __("Dirección:")}}</td>
-            <td class="td-content">{{ $client->address }}</td>
+                <td class="table-dark td-title">{{ __("Celular:")}}</td>
+                <td class="td-content">{{ $client->cell_phone_number }}</td>
+            </tr>
+            <tr>
+                <td class="table-dark td-title">{{ __("Dirección:")}}</td>
+                <td class="td-content">{{ $client->address }}</td>
 
-            <td class="table-dark td-title">{{ __("Correo electrónico:")}}</td>
-            <td class="td-content">{{ $client->email }}</td>
-        </tr>
-        <tr>
-            <td class="table-dark td-title">{{ __("Facturas:")}}</td>
-            <td class="td-content">
-                @if($client->invoices->isEmpty())
-                    {{ __("Sin facturas asociadas")}}
-                @else
-                    <ul>
-                        @foreach($client->invoices as $invoice)
-                            <li>
-                                <a href="{{ route('invoices.show', $invoice) }}" target="_blank">
-                                    {{ __("Factura de venta No.")}} {{ $invoice->id }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                @endisset
-            </td>
-        </tr>
-    </table>
+                <td class="table-dark td-title">{{ __("Correo electrónico:")}}</td>
+                <td class="td-content">{{ $client->email }}</td>
+            </tr>
+        </table>
+    </div>
+    <br>
+    <div class="shadow">
+        <div class="card-header text-center"><h3>{{ __("Facturas asociadas") }}</h3></div>
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th>{{ __("Título") }}</th>
+                    <th>{{ __("Fecha de expedición") }}</th>
+                    <th>{{ __("Estado") }}</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($client->invoices as $invoice)
+                <tr>
+                    <td>
+                        <a href="{{ route('invoices.show', $invoice) }}" target="_blank">
+                            {{ __("Factura de venta No.")}} {{ $invoice->id }}
+                        </a>
+                    </td>
+                    <td>{{ $invoice->issued_at }}</td>
+                    @include('invoices.status_label')
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.show')
 @section('Title', 'Ver Producto')
 @section('Back')
-    <a href="{{ route('products.index') }}" class="btn btn-secondary">
-        <i class="fa fa-arrow-left"></i> {{ __("Volver") }}
-    </a>
+    <div>
+        <a href="{{ route('products.index') }}" class="btn btn-secondary">
+            <i class="fa fa-arrow-left"></i> {{ __("Volver") }}
+        </a>
+    </div>
+    <div>
+        <a class="btn btn-success" href="{{ route('products.create') }}">
+            <i class="fa fa-plus"></i> {{ __("Crear nuevo producto") }}
+        </a>
+    </div>
 @endsection
 @section('Name')
     {{ $product->name }}
@@ -12,24 +19,27 @@
     @include('products._buttons')
 @endsection
 @section('Body')
-    <table class="table border-rounded table-sm">
-        <tr>
-            <td class="table-dark td-title">{{ __("Nombre:") }}</td>
-            <td class="td-content">{{ $product->name }}</td>
+    <div class="shadow">
+        <div class="card-header text-center"><h3>{{ __("Datos generales") }}</h3></div>
+        <table class="table border-rounded table-sm">
+            <tr>
+                <td class="table-dark td-title">{{ __("Nombre:") }}</td>
+                <td class="td-content">{{ $product->name }}</td>
 
-            <td class="table-dark td-title">{{ __("Precio unitario:") }}</td>
-            <td class="td-content">{{ $product->unit_price }}</td>
-        </tr>
-        <tr>
-            <td class="table-dark td-title">{{ __("Creado:") }}</td>
-            <td class="td-content">{{ $product->created_at }}</td>
+                <td class="table-dark td-title">{{ __("Precio unitario:") }}</td>
+                <td class="td-content">${{ number_format($product->unit_price, 2) }}</td>
+            </tr>
+            <tr>
+                <td class="table-dark td-title">{{ __("Creado:") }}</td>
+                <td class="td-content">{{ $product->created_at->isoFormat('Y-MM-DD hh:mma') }}</td>
 
-            <td class="table-dark td-title">{{ __("Modificado:") }}</td>
-            <td class="td-content">{{ $product->updated_at }}</td>
-        </tr>
-        <tr>
-            <td class="table-dark td-title">{{ __("Descripción:") }}</td>
-            <td class="td-content">{{ $product->description }}</td>
-        </tr>
-    </table>
+                <td class="table-dark td-title">{{ __("Modificado:") }}</td>
+                <td class="td-content">{{ $product->updated_at->isoFormat('Y-MM-DD hh:mma') }}</td>
+            </tr>
+            <tr>
+                <td class="table-dark td-title">{{ __("Descripción:") }}</td>
+                <td class="td-content">{{ $product->description }}</td>
+            </tr>
+        </table>
+    </div>
 @endsection
