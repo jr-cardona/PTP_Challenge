dafd5142dbebaa2ac0fb8586ae7c93065b2db5e8
Juan Ricardo <jucardonaa@unal.edu.co>
2020-02-16 17:30:10 -0500

    Refactored Products Test

---
+++ tests/Feature/Admin/Products/ExcelProductTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Admin\Products;
+
+use App\User;
+use Tests\TestCase;
+use App\Exports\ProductsExport;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExcelProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function user_can_download_products_export()
+    {
+        $user = factory(User::class)->create();
+        Excel::fake();
+
+        $this->actingAs($user)->get(route('export.products'));
+
+        Excel::assertDownloaded('products-list.xlsx', function(ProductsExport $export) {
+            return $export->view()->name() == 'exports.products';
+        });
+    }
+}
