64e52e7131e9baa8f9b491349ef8a5e896b244d7
Juan Ricardo <jucardonaa@unal.edu.co>
2020-03-23 03:40:26 -0500

    Errors views modifications.

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,16 +1,16 @@
-@extends('errors.app')
+@extends('layouts.errors')
 @section('title', '403')
 @section('body')
     <div class="container">
         <div class="row text-center">
-            <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3 col-12 p-3 error-main">
+            <div class="col p-3 error-main">
                 <div class="row">
                     <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
                         <h1 class="m-0">403</h1>
-                        <h6>{{ __("Página no autorizada | RetoPTP") }}</h6>
-                        <p>
+                        <h2>{{ __("Página no autorizada | RetoPTP") }}</h2>
+                        <h4>
                             {{ __("Regresar a ") }}<a href="{{ route('home') }}">{{ __("Inicio") }}</a>
-                        </p>
+                        </h4>
                     </div>
                 </div>
             </div>
