5424ad1ce9ba218bae17f55d11c9d57b7eca2fab
Juan Ricardo <jucardonaa@unal.edu.co>
2020-04-07 06:07:46 -0500

    Observers corrections.

--- app/Observers/ClientsObserver.php
+++ app/Observers/ClientsObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Entities\User;
 use App\Entities\Client;
 
 class ClientsObserver
@@ -9,7 +10,7 @@ class ClientsObserver
     public function creating(Client $client)
     {
         if (! $client->created_by) {
-            $client->created_by = auth()->user()->id;
+            $client->created_by = auth()->user()->id ?? User::first()->id;
         }
         $client->updated_by = $client->created_by;
     }
--- app/Observers/UsersObserver.php
+++ app/Observers/UsersObserver.php
@@ -9,16 +9,14 @@ class UsersObserver
     public function creating(User $user)
     {
         $user->password = 'secret';
-    }
-
-    public function created(User $user)
-    {
         $user->created_by = auth()->user()->id ?? User::first()->id ?? null;
         $user->updated_by = $user->created_by;
     }
 
-    public function updated(User $user)
+    public function updating(User $user)
     {
-        $user->updated_by = auth()->user()->id;
+        if (auth()->user()) {
+            $user->updated_by = auth()->user()->id;
+        }
     }
 }
