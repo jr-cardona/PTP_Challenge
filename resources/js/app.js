32ff220cddda879916373481c8affe1912b0bf56
Juan Ricardo <jucardonaa@unal.edu.co>
2020-04-07 06:13:54 -0500

    Js corrections.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -11,21 +5,10 @@ window.Vue = require('vue');
 import Vue from 'vue';
 import 'vue-select/dist/vue-select.css';
 import VueSelect from 'vue-select/dist/vue-select.js';
+import Notifications from "./components/Notifications";
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 Vue.component('v-select', VueSelect);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.component('notifications', Notifications);
 
 const app = new Vue({
     el: "#app",
--- resources/js/import-modal.js
+++ resources/js/import-modal.js
@@ -1,5 +1,5 @@
 $('#importModal').on('show.bs.modal', function (e) {
     $('#model').attr('value', $(e.relatedTarget).data('model'));
     $('#redirect').attr('value', $(e.relatedTarget).data('redirect'));
-    $('#import-model').attr('value', $(e.relatedTarget).data('import-model'));
+    $('#import_model').attr('value', $(e.relatedTarget).data('import_model'));
 });
