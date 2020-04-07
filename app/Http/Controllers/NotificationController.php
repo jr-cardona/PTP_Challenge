2fc0ae6c35f7323d2608d33bceb817eae7a5e68c
Juan Ricardo <jucardonaa@unal.edu.co>
2020-04-07 06:03:22 -0500

    Create NotificationController.php

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Notifications\DatabaseNotification;
+
+class NotificationController extends Controller
+{
+    public function __construct()
+    {
+        if (! request()->ajax()){
+            return redirect()->back()->with('info', 'Acción no válida');
+        }
+    }
+
+    public function index() {
+        return [
+            'notifications' => auth()->user()->notifications,
+            'unreadNotifications' => count(auth()->user()->unreadNotifications),
+        ];
+    }
+
+    public function read($id){
+        DatabaseNotification::find($id)->markAsRead();
+        return auth()->user()->notifications;
+    }
+
+    public function destroy($id){
+        DatabaseNotification::find($id)->delete();
+    }
+}
